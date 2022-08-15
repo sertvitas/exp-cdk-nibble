11cc0d982c1239cfc44e0f09a75249c07cb4ec6f
Moon <erik.moon@imprivata.com>
2022-08-15 14:29:55 -0400

    simple vpc build

--- exp_cdk_nibble/exp_cdk_nibble_stack.py
+++ exp_cdk_nibble/exp_cdk_nibble_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
     Duration,
     Stack,
-    aws_sqs as sqs,
+    aws_ec2 as ec2,
 )
 from constructs import Construct
 
@@ -10,11 +10,8 @@ class ExpCdkNibbleStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
-
-        # example resource
-        queue = sqs.Queue(
-            self, "ExpCdkNibbleQueue",
-            visibility_timeout=Duration.seconds(300),
-        )
+        vpc = ec2.Vpc(self, "TheVPC",
+                      cidr="10.0.0.0/16"
+                      )
+ = ec2.Vpc(self, "TheVPC",
+                      cidr="10.0.0.0/16"
