058b9601805fc1cdf0241e66efb5efe3ea3513f4
Moon <erik.moon@imprivata.com>
2022-08-24 10:12:55 -0400

    added engine 11

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import aws_cdk as cdk
 
 from exp_cdk_nibble.connectivity import ExpCdkNibbleStack
 from exp_cdk_nibble.rds import RdsStack
+from exp_cdk_nibble.rds_11 import Rds11Stack
 
 #default_env = cdk.Environment(account=AWS_ACCOUNT_NUMBER, region=DEFAULT_REGION)
 
@@ -12,4 +13,6 @@ app = cdk.App()
 network_stack = ExpCdkNibbleStack(app, "ExpCdkNibbleStack")
 rds_stack = RdsStack(app, "RdsStack",network_stack.vpc)
 rds_stack.add_dependency(network_stack)
+rds11_stack = Rds11Stack(app, "Rds11Stack",network_stack.vpc)
+rds11_stack.add_dependency(network_stack)
 app.synth()
---
+++ exp_cdk_nibble/rds_11.py
@@ -0,0 +1,77 @@
+import json
+from aws_cdk import (
+    Stack,
+    aws_secretsmanager as sm,
+    aws_rds as rds,
+    aws_ec2 as ec2,
+)
+from constructs import Construct
+
+
+class Rds11Stack(Stack):
+    def __init__(
+            self, scope: Construct, construct_id: str, target_vpc, **kwargs
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        my_secret = sm.Secret(
+            self,
+            "Secret",
+            generate_secret_string=sm.SecretStringGenerator(
+                secret_string_template=json.dumps(
+                    {"username": "postgres"}, separators=(",", ":")
+                ),
+                generate_string_key="password",
+                exclude_punctuation=True
+            ),
+        )
+        engine = rds.DatabaseInstanceEngine.postgres(version=rds.PostgresEngineVersion.VER_11_13)
+        parameter_group = rds.ParameterGroup(
+            self,
+            "ParameterGroup",
+            engine=engine,
+            parameters={
+                "rds.logical_replication": "1",
+                "autovacuum_naptime": "40",
+                # rds.allowed_extensions requires ver 12.6+ only.
+                #"rds.allowed_extensions": "dblink, hstore, pg_stat_statements",
+                "wal_sender_timeout": "0",
+                "shared_preload_libraries": "pg_stat_statements"
+            }
+        )
+        instance1 = rds.DatabaseInstance(
+            self,
+            "PostgresInstance1",
+            engine=engine,
+            parameter_group = parameter_group,
+            credentials=rds.Credentials.from_secret(my_secret),
+            vpc=target_vpc,
+            allocated_storage=100,
+            allow_major_version_upgrade=False,
+            auto_minor_version_upgrade=False,
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.T3, ec2.InstanceSize.SMALL
+            ),
+            # backup_retention=,
+            copy_tags_to_snapshot=True,
+            deletion_protection=False,
+            enable_performance_insights=True,
+            multi_az=True,
+            # parameter_group=,
+            # preferred_backup_window=,
+            # preferred_maintenance_window=,
+            # removal_policy=,
+            # storage_type=,
+            vpc_subnets=ec2.SubnetSelection(
+                subnet_type=ec2.SubnetType.PRIVATE_ISOLATED
+            ),
+        )
+        sm.SecretRotation(
+            self,
+            "SecretRotation",
+            application=sm.SecretRotationApplication.POSTGRES_ROTATION_SINGLE_USER,
+            # Postgres single user scheme
+            secret=my_secret,
+            target=instance1,  # a Connectable
+            vpc=target_vpc,  # The VPC where the secret rotation application will be deployed
+            exclude_characters=" %+:;\{\}'\"\,@\\",
+        )
