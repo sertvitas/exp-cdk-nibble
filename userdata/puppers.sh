c2caec14f00ce48ef2c2b7138e74f37a88138b6c
Moon <erik.moon@imprivata.com>
2022-09-09 07:09:03 -0400

    Updates to puppers versioning, adding CloudWatch agent, AWS cli, and other utilities.

--- userdata/puppers.sh
+++ userdata/puppers.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
+
+echo "Userdata script did run" >> /tmp/script_confirmation.txt
+yum install -y amazon-cloudwatch-agent
+amazon-cloudwatch-agent-ctl -a start
+
+yum install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 echo "PUPPERS"
+
+useradd -m -d /opt/puppers -s /bin/bash puppers
+usermod  -L puppers
+
 touch /tmp/puppers.txt
-curl -Lo /opt/puppers_0.0.3_linux_amd64.tar.gz \
-https://github.com/natemarks/puppers/releases/download/v0.0.3/puppers_0.0.3_linux_amd64.tar.gz
+curl -Lo /opt/puppers_0.0.5_linux_amd64.tar.gz \
+https://github.com/natemarks/puppers/releases/download/v0.0.5/puppers_0.0.5_linux_amd64.tar.gz
 
 mkdir -p /opt/puppers
 
-tar -xzvf /opt/puppers_0.0.3_linux_amd64.tar.gz -C /opt/puppers
+tar -xzvf /opt/puppers_0.0.5_linux_amd64.tar.gz -C /opt/puppers
 
-nohup /opt/puppers/puppers &
+chown -R puppers:puppers /opt/puppers
+
+runuser -l puppers -c "AWS_REGION=us-east-1 PUPPERS_SECRET_NAME=SecretA720EF05-YA1uGeVL9JKx nohup /opt/puppers/puppers &"
+
